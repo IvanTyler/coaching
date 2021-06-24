11197af2014a29962e745a8369398d78afda679c
Zaur <Zulpikarov.Z@gmail.com>
2021-06-24 14:44:37 +0300

    upload ready auth

--- db/models/userModel.js
+++ db/models/userModel.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose')
 
 const usersSchema = mongoose.Schema({
-    name: {
+    firstName: {
         type: String,
         required: true,
     },
+    lastName: {
+      type: String,
+      required: true,
+  },
     email: {
         type: String,
         required: true,
